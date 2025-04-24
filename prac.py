a72ae54e472a09adcb09f6ce647c8f986acdf2fc
Deepak Rathore <rathoredeepak8439@gmail.com>
2025-04-24 08:23:51 +0530

    add new file prac.py

---
+++ prac.py
@@ -0,0 +1,88 @@
+#sum of two number 
+
+# a = 2
+# b = 4
+# sum = a + b
+# print(f"the sum of {a} and {b} is {sum}")
+
+
+# subtraction of two number
+# a=2
+# b=5
+# sub= a-b
+# print(f"the subtracrtion of {a} and {b} is {sub}")
+
+# print(f"the divide of {a} and {b} is {a/b}")
+# multi = a*b
+# print(f"the multiply of {a} and {b} is {multi}")
+
+#expression execution 
+# str  and numeric values can operate together with * operator
+
+# A,B = 2,3
+# Txt = "@"
+
+# print(A*Txt*B)
+
+# print(2*"@"*3)
+# print(2*Txt*3)
+
+
+# # str and str can operate with + operater
+
+# a,b ="2",3
+# txt ="@"
+# print((a+txt)*b)
+
+#numeric  values can operate with all arithmetic operator 
+
+# a , b = 2,3
+# c =4
+# print((a+b*c))
+
+#arithmetic expression with integer and float will result in float
+
+# a , b = 10 , 5.34
+# c = a*b
+# print(c)
+# print(a+b)
+# print(type(c))
+
+#result of division operator  with two integers will be float
+
+# a,b=1,2
+# c=a/b
+# print(c)
+
+#integer division with float and integer will give int displayed as float
+
+# a,b =1.4,4
+# c=a//b
+# print(c)
+
+# floor gives closest integer which is lesser than or equal to the float value result of a//b is a same as floor a/b
+# a,b =16,5
+# c=a//b
+# print(c)
+
+# a,b =-16,5
+# c=a//b
+# print(c)
+
+# a,b =16,-5
+# c=a//b
+# print(c)
+
+# reminder is (-ve ) when denominator is (-ve)
+
+# a,b =-16,5
+# c=a/b
+# print(c)
+
+# a,b =16,5
+# c=a/b
+# print(c)
+
+# a,b =16,-5
+# c=a/b
+# print(c)
